3c9416c17a711918d8825d88b3504fb83718dd61
chrissydc <bychriscelle@gmail.com>
2018-06-17 23:51:39 -0400

    basic html added

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+// create a countdown timer of 120 seconds
+
+
+
+// game ends when the time runs out
+
+
+
+// create a trivia form with multiple choice or true/false options (10 questions)
+
+// FOR LOOP - go through each multiple choice to assess if selected is correct or wrong
+
+
+
+// player can only pick one answer per question
+
+
+
+
+// page reveals questions answered correctly
+// will show once timer runs out
+
+
+// page reveals questions answered incorrectly
+// will show once timer runs out
+
+
+
